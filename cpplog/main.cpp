237310a59ddfa5ed79bb25f44c823c0726fb7d3a
fbh <fbhfyc@163.com>
2025-07-21 06:45:43 +0000

    df

--- cpplog/main.cpp
+++ cpplog/main.cpp
@@ -7,7 +7,7 @@ using namespace yazi::utility;
 
 int main()
 {
-    Logger::instance()->open("./test.log");
+    Logger::instance()->open("test.log");
     Logger::instance()->set_max(100);
     for(int i=0; i<100; ++i) {
     	Logger::instance()->log(Logger::DEBUG,__FILE__,__LINE__,"hello world","fbh");
